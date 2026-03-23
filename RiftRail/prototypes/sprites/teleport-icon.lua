7f61275f81dd368af2621f0f1302bde17789c446
VariousTrick <wangfan00987@gmail.com>
2026-03-23 23:52:18 +0800

    传送图标

---
+++ RiftRail/prototypes/sprites/teleport-icon.lua
@@ -0,0 +1,10 @@
+data:extend({
+    {
+        type = "sprite",
+        name = "riftrail-teleport-player-icon",
+        filename = "__RiftRail__/graphics/icon/run.png",
+        width = 64,
+        height = 64,
+        flags = { "gui-icon" },
+    },
+})
